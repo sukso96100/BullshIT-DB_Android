bd9751da4396dace958509cc1775e2660eeef9f0
sukso96100 <sukso96100@gmail.com>
2015-02-23 14:56:40 +0900

    added sms alert

--- app/src/main/java/org/bullshitbankdb/android/SmsReceiver.java
+++ app/src/main/java/org/bullshitbankdb/android/SmsReceiver.java
@@ -6,39 +6,45 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.Preference;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
-import android.telecom.TelecomManager;
+import android.telephony.SmsMessage;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.SaveCallback;
 
-import java.util.List;
-
-public class PhoneCallReceiver extends BroadcastReceiver {
-    String TAG = "PhoneCallReceiver";
-    public PhoneCallReceiver() {
+public class SmsReceiver extends BroadcastReceiver {
+    String PhoneNumber;
+    String TAG = "SmsReceiver";
+    public SmsReceiver() {
     }
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
 
-        // If phone state is "Rininging"
-        if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+        //---get the SMS message passed in---
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        if (bundle != null){
+            //---retrieve the SMS message received---
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i=0; i<msgs.length; i++){
+                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                PhoneNumber = msgs[i].getOriginatingAddress();
+                Log.d(TAG, "Phone Number: " + PhoneNumber);
+            }
+
             SharedPreferences Pref = PreferenceManager.getDefaultSharedPreferences(context);
-            Boolean PhoneCallAlert = Pref.getBoolean("call_alert",true);
-            final String PhoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-            if(PhoneCallAlert){
-                Log.d(TAG,"Phone Number: "+PhoneNumber);
+            Boolean SmsAlert = Pref.getBoolean("sms_alert",true);
+            if(SmsAlert){
+                
                 Intent myIntent = new Intent(context, DetilActivity.class);
                 myIntent.putExtra("phone",PhoneNumber);
                 final PendingIntent pendingIntent
@@ -57,7 +63,8 @@ public class PhoneCallReceiver extends BroadcastReceiver {
                                         .setContentText(context.getResources().getString(R.string.returns_true))
                                         .setContentIntent(pendingIntent)
                                         .setSmallIcon(R.drawable.ic_noti_bullshitbank);
-                                NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+                                NotificationManager notificationManager
+                                        = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
                                 notificationManager.notify(0,builder.build());
                                 Intent alertIntent = new Intent(context, BullshITAlertService.class);
                                 alertIntent.putExtra("phone",PhoneNumber);
@@ -76,5 +83,7 @@ public class PhoneCallReceiver extends BroadcastReceiver {
         }
 
 
+
+    
     }
 }
